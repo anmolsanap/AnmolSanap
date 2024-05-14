40d3a192da727339b47e6d479e5376ec7f15ebd8
anmolsanap <158318618+anmolsanap@users.noreply.github.com>
2024-05-14 16:36:15 +0530

    Update AmolDemo.java

--- MySimpleProject/src/com/model/AmolDemo.java
+++ MySimpleProject/src/com/model/AmolDemo.java
@@ -11,7 +11,7 @@ public class AmolDemo {
 	public static void main(String args[])
 	{
 		System.out.println("main method--AmolDemo");
-
+	}
 	public void m2()
 	{
 		System.out.println("m2--AmolDemo");
